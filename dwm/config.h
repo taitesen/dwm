c7c935355112b86143a89c59ba5d5306f3a14c9a
taitesen <iamhmunhlei@gmail.com>
2024-08-29 19:24:49 +0530

    bar icons

--- dwm/config.h
+++ dwm/config.h
@@ -31,7 +31,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
