c8b1c5bcf399f441648826d01c860e8b71c2aaa7
taitesen <iamhmunhlei@gmail.com>
2024-08-30 18:00:40 +0530

    update before applying dmenu lineheight patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -34,7 +34,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 /*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
-static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
