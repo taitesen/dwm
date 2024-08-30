23e81c23d9b112613a4ea1aaf336d6e9d9e3bac6
taitesen <iamhmunhlei@gmail.com>
2024-08-30 18:28:31 +0530

    applied dmenu alpha patch

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0x00;     /* Amount of opacity. 0xff is opaque             */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
@@ -17,6 +18,12 @@ static const char *colors[SchemeLast][2] = {
     [SchemeNorm]  =  {"#eabbb9",  "#000000"},
     [SchemeSel]   =  {"#000000",  "#eabbb9"},
     [SchemeOut]   =  {"#000000",  "#d79921"},
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 3;
