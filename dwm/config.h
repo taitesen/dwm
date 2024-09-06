e34c31b9a1abcaf28556124255061e9403696a70
taitesen <iamhmunhlei@gmail.com>
2024-09-06 20:04:34 +0530

    skadosh

--- dwm/config.h
+++ dwm/config.h
@@ -4,9 +4,9 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
@@ -16,7 +16,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 30;       /* horizontal padding of bar */
 
-static const int horizpadbar        = 1;        /* horizontal padding for statusbar */
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 20;        /* vertical padding for statusbar */
 
 static const char *fonts[]          = { "Terminess Nerd Font:size=16:style=Bold:antialias=true:autohint=true", "Font Awesome 6 Free:size=14:antialias=true:autohint=true"};
