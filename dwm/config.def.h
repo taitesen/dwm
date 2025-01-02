48210c2e60965151e9d4ea2e999d212b51d2eef0
taitesen <iamhmunhlei@gmail.com>
2025-01-02 19:32:21 +0530

    anada wan

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 */
 	/* class                         instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
 	{ "Gimp",                        NULL,       NULL,       0,            1,           -1,        50,50,500,500,        -1 },
-	{ "zen-alpha",                   NULL,       NULL,       1 << 1,       0,           -1,        50,50,500,500,        -1 },
+	{ "zen-beta",                    NULL,       NULL,       1 << 1,       0,           -1,        50,50,500,500,        -1 },
 	{ "qutebrowser",                 NULL,       NULL,       1 << 2,       0,           -1,        50,50,500,500,        -1 },
 	{ "Zathura",                     NULL,       NULL,       1 << 3,       0,           -1,        50,50,500,500,        -1 },
 	{ "eu.betterbird.Betterbird",    NULL,       NULL,       1 << 4,       0,           -1,        50,50,500,500,        -1 },
