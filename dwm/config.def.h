14d085797bcdd3e6ebc4950a5d76c9d52c570f07
taitesen <iamhmunhlei@gmail.com>
2025-05-29 03:04:01 +0530

    back to firefox

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class                         instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
 	{ "Gimp",                        NULL,       NULL,       0,            1,           -1,        50,50,500,500,        -1 },
-	{ "zen-beta",                    NULL,       NULL,       1 << 1,       0,           -1,        50,50,500,500,        -1 },
+	{ "firefox",                     NULL,       NULL,       1 << 1,       0,           -1,        50,50,500,500,        -1 },
 	{ "qutebrowser",                 NULL,       NULL,       1 << 2,       0,           -1,        210,45,1500,1030,     -1 },
 	{ "Zathura",                     NULL,       NULL,       1 << 3,       0,           -1,        50,50,500,500,        -1 },
 	{ "sioyek",                      NULL,       NULL,       1 << 3,       0,           -1,        50,50,500,500,        -1 },
